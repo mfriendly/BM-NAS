9e2cf4e199028b657d531e9dd8e8a657495f433e
MinKim <96563294+mfriendly@users.noreply.github.com>
2022-04-14 10:16:38 +0900

    Delete _033_SP500_two directory

--- _033_SP500_two/txtcsv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-root_dir = os.getcwd()
-print( root_dir )
-file = pd.read_csv(root_dir + '/log_search.txt')
-new_csv_file = file.to_csv(root_dir + '/log_search.csv')
-
-file2 = pd.read_csv(root_dir + '/log_final.txt')
-new_csv_file = file2.to_csv(root_dir + '/log_final.csv')
